4826c583c4e88d4f03daafb6ce6d70bfbe6372ae
ljbrewer <ljbrewer.lb@gmail.com>
2021-09-03 15:08:16 -0700

    All done except the delete function

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const path = require('path');
 const { nanoid } = require('nanoid');
 
 
+
 const app = express();
-const PORT =process.env.PORT || 3003;
+const PORT = process.env.PORT || 3003;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,25 +16,36 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Routes
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname,'public', 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
 
 app.get('/api/notes', (req, res) => res.json(notesData));
 
 
 app.post('/api/notes', (req, res) => {
-       // req.body is available since we're using the body parsing middleware
-     
-       notesData.push(req.body)
-        req.body.id = nanoid()
-       fs.writeFile('./db/db.json', JSON.stringify(notesData), err =>{
-        if (err) throw err;
-       }) 
-      // req.body.id = nanoid()
-      
-       res.json(req.body)
+        // req.body is available since we're using the body parsing middleware
 
-        }
+        notesData.push(req.body)
+        req.body.id = nanoid()
+        fs.writeFile('./db/db.json', JSON.stringify(notesData), err => {
+                if (err) throw err;
+        })
+        // req.body.id = nanoid()
+
+        res.json(req.body)
+
+}
 )
+// app.delete("/api/notes/:id", (req, res) => {
+//         // (1) Get the currently saved array of notes
+//         const { id } = req.params;
+//         // (2) Remove the note referenced by the ID (req.params.id) passed in from the request
+//         const updatenotesData = notesData.filter(id => notesData.id);
+//         const notesData = notesData.splice(id, 1)
+//         // (3) Save the new array back to db.json
+//         db.json = notesData
+// });
+
+
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 // Listener
 
