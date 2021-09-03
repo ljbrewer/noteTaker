b355ee8a736a982cb59bc1f9b4ae6b37c522063d
ljbrewer <ljbrewer.lb@gmail.com>
2021-09-03 11:39:47 -0700

    copy files from dev folder

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const PORT =process.env.PORT || 3003;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static('public'));
 
 // Routes
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname,'public', 'notes.html')));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+
 
 
 app.get('/api/notes', (req, res) => res.json(notesData));
@@ -32,7 +32,7 @@ app.post('/api/notes', (req, res) => {
 
         }
 )
-
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 // Listener
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
