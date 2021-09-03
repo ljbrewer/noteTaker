0365adce4ad750f4917b35898517d3947c0e55e2
ljbrewer <ljbrewer.lb@gmail.com>
2021-09-03 15:17:24 -0700

    commit before deploying

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
 
 app.get('/api/notes', (req, res) => res.json(notesData));
